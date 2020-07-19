2553e0135cbcd5a67abddbca3ba0c11158eac5bb
Romain Beguet <beguet@adacore.com>
2020-07-19 17:40:44 +0200

    Use single rewriting session.

--- src/main.adb
+++ src/main.adb
@@ -25,29 +25,9 @@ procedure Main is
       Description  => "Garbage collection for Ada",
       Process_Unit => Process_Unit);
 
-   procedure Handle_Allocator (Node : LAL.Allocator'Class)
+   procedure Add_With_Clause
+     (RH : LALRW.Rewriting_Handle; Unit : LAL.Compilation_Unit'Class)
    is
-      Ctx : LAL.Analysis_Context := Node.Unit.Context;
-      RH  : LALRW.Rewriting_Handle := LALRW.Start_Rewriting (Ctx);
-      SH  : LALRW.Node_Rewriting_Handle := LALRW.Handle (Node);
-   begin
-      LALRW.Replace
-        (SH, LALRW.Create_Call_Expr
-           (RH,
-            LALRW.Create_Dotted_Name
-              (RH,
-               LALRW.Create_Token_Node (RH, LALCO.Ada_Identifier, "GC"),
-               LALRW.Create_Token_Node (RH, LALCO.Ada_Identifier, "Register")),
-            LALRW.Clone (SH)));
-      if not LALRW.Apply (RH).Success then
-         raise Program_Error with "Could not rewrite unit";
-      end if;
-   end Handle_Allocator;
-
-   procedure Add_With_Clause (Unit : LAL.Compilation_Unit'Class)
-   is
-      Ctx : LAL.Analysis_Context := Unit.Unit.Context;
-      RH  : LALRW.Rewriting_Handle := LALRW.Start_Rewriting (Ctx);
       PH  : LALRW.Node_Rewriting_Handle := LALRW.Handle (Unit.F_Prelude);
    begin
       LALRW.Insert_Child
@@ -60,21 +40,35 @@ procedure Main is
               (RH, LALCO.Ada_Name_List,
                (1 => LALRW.Create_Token_Node
                   (RH, LALCO.Ada_Identifier, "GC")))));
-      if not LALRW.Apply (RH).Success then
-         raise Program_Error with "Could not add with clause";
-      end if;
    end Add_With_Clause;
+
+   procedure Handle_Allocator
+     (RH : LALRW.Rewriting_Handle; Node : LAL.Allocator'Class)
+   is
+      SH  : LALRW.Node_Rewriting_Handle := LALRW.Handle (Node);
+   begin
+      LALRW.Replace
+        (SH, LALRW.Create_Call_Expr
+           (RH,
+            LALRW.Create_Dotted_Name
+              (RH,
+               LALRW.Create_Token_Node (RH, LALCO.Ada_Identifier, "GC"),
+               LALRW.Create_Token_Node (RH, LALCO.Ada_Identifier, "Register")),
+            LALRW.Clone (SH)));
+   end Handle_Allocator;
 
    procedure Process_Unit
      (Job_Ctx : Helpers.App_Job_Context; Unit : LAL.Analysis_Unit)
    is
+      RH : LALRW.Rewriting_Handle := LALRW.Start_Rewriting (Unit.Context);
+
       function Process_Node
         (Node : LAL.Ada_Node'Class) return LALCO.Visit_Status
       is
       begin
          case Node.Kind is
             when LALCO.Ada_Allocator =>
-               Handle_Allocator (Node.As_Allocator);
+               Handle_Allocator (RH, Node.As_Allocator);
             when others =>
                null;
          end case;
@@ -87,13 +81,17 @@ procedure Main is
          raise Program_Error with "Unhandled multi compilation unit files";
       end if;
 
-      Add_With_Clause (Unit.Root.As_Compilation_Unit);
+      Add_With_Clause (RH, Unit.Root.As_Compilation_Unit);
 
       if Unit.Has_Diagnostics then
          Put_Line ("Invalid ada unit " & Unit.Get_Filename);
       else
          Unit.Root.Traverse (Process_Node'Access);
-         Put_Line (LALU.Unparse (Unit.Root));
+         if LALRW.Apply (RH).Success then
+            Put_Line (LALU.Unparse (Unit.Root));
+         else
+            raise Program_Error with "Could not apply rewritings";
+         end if;
       end if;
    end Process_Unit;
 begin
