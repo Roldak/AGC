ce52bcaf0757d85a94f1ecf6e0ab809ada1fb116
Romain Beguet <beguet@adacore.com>
2020-07-21 17:01:44 +0200

    Add simple recursion test.

---
+++ testsuite/tests/simple_recursion.test/test.adb
@@ -0,0 +1,16 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Test is
+   type Integer_Access is access all Integer;
+
+   procedure Foo (N : Natural) is
+      X : Integer_Access := new Integer'(42);
+   begin
+      if N < 100 then
+         Foo (N + 1);
+      end if;
+   end Foo;
+begin
+   Foo (0);
+   Foo (100);
+end Test;
