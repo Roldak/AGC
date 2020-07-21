219a7440a34f72a793532095f995d226f7f12fd8
Romain Beguet <beguet@adacore.com>
2020-07-22 00:27:55 +0200

    Rewrite valgrind runner.

--- testsuite/runners/valgrind.runner/run.py
+++ testsuite/runners/valgrind.runner/run.py
@@ -8,9 +8,6 @@ gpr_template = """
 with "agc_runtime";
 project Test is
     for Main use ("{}");
-    for Source_Dirs use ("{}");
-    for Object_Dir use "{}";
-    for Exec_Dir use "{}";
 end Test;
 """
 
@@ -22,32 +19,42 @@ def format_valgrind_output(output):
 
 
 def run(src_file):
-    out = subprocess.run(["agc", "test.adb"], capture_output=True)
     with tempfile.TemporaryDirectory() as d:
-        with tempfile.NamedTemporaryFile(suffix=".adb", dir=d) as src:
-            src.write(out.stdout)
-            src.flush()
-            with tempfile.NamedTemporaryFile(suffix=".gpr") as gpr:
-                gpr_content = gpr_template.format(
-                    src.name, d, d, d, src.name
-                ).encode()
-                gpr.write(gpr_content)
-                gpr.flush()
-                gprbuild_out = subprocess.run(
-                    ["gprbuild", "-P", gpr.name],
-                    capture_output=True,
-                    text=True
-                )
-                if gprbuild_out.returncode != 0:
-                    print(gprbuild_out.stderr)
-                else:
-                    valgrind_out = subprocess.run(
-                        ["valgrind",
-                         os.path.join(d, src.name[:-4])],
-                        capture_output=True,
-                        text=True
-                    )
-                    print(format_valgrind_output(valgrind_out.stderr))
+        # Run AGC
+        agc_out = subprocess.run(
+            ["agc", src_file, "--output-dir", d],
+            capture_output=True,
+            text=True
+        )
+        if agc_out.returncode != 0:
+            print(agc_out.stderr)
+            return
+
+        # Create GPR project file
+        gpr_path = os.path.join(d, "test.gpr")
+        with open(gpr_path, "wb") as gpr:
+            gpr_content = gpr_template.format(src_file).encode()
+            gpr.write(gpr_content)
+            gpr.flush()
+
+        # Build generated project
+        gprbuild_out = subprocess.run(
+            ["gprbuild", "-P", gpr_path],
+            capture_output=True,
+            text=True
+        )
+        if gprbuild_out.returncode != 0:
+            print(gprbuild_out.stderr)
+            return
+
+        # Run valgrind on resulting binary
+        valgrind_out = subprocess.run(
+            ["valgrind",
+             os.path.join(d, src_file[:-4])],
+            capture_output=True,
+            text=True
+        )
+        print(format_valgrind_output(valgrind_out.stderr))
 
 
 if __name__ == "__main__":
