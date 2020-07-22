8fbe7e3f3df7bdab516ffacd835887e3bb406838
Romain Beguet <beguet@adacore.com>
2020-07-22 22:44:42 +0200

    Add test with multiple return stmts.

---
+++ testsuite/tests/multiple_return_stmts.test/test.adb
@@ -0,0 +1,18 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Test is
+   type Integer_Access is access all Integer;
+
+   function Inner (B : Boolean; X : Integer) return Integer_Access is
+   begin
+      if B then
+         return new Integer'(X + 1);
+      else
+         return new Integer'(X - 1);
+      end if;
+   end Inner;
+
+   A : Integer := Inner (True, 42).all;
+begin
+   GC.Collect;
+end Test;
