e6af78657139f7a5ca259c62fa67554126780ec5
Romain Beguet <beguet@adacore.com>
2020-07-29 09:33:49 +0200

    Extend GC.Standard.

--- lib/src/gc-standard.ads
+++ lib/src/gc-standard.ads
@@ -1,4 +1,6 @@
 package GC.Standard is
-   function  AGC_Register             is new GC.Register (Integer);
-   procedure Visitor_Standard_Integer is new GC.No_Op (Integer);
+   function  AGC_Register               is new GC.Register (Integer);
+   procedure Visitor_Standard_Integer   is new GC.No_Op (Integer);
+   procedure Visitor_Standard_Character is new GC.No_Op (Character);
+   procedure Visitor_Standard_String    is new GC.No_Op (String);
 end GC.Standard;
