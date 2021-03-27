bf017c9850ff11f0c946e1b366e2df5f31081b1a
Romain Beguet <beguet@adacore.com>
2021-03-27 13:09:41 +0100

    Add test exercising handling of external projects.

---
+++ testsuite/tests/library_types_1.test/lib/lib_types.ads
@@ -0,0 +1,3 @@
+package Lib_Types is
+   type Integer_Access is access all Integer;
+end Lib_Types;
---
+++ testsuite/tests/library_types_1.test/main.adb
@@ -0,0 +1,7 @@
+with Lib_Types;
+
+procedure Main is
+   X : Lib_Types.Integer_Access := new Integer'(2);
+begin
+   null;
+end Main;
